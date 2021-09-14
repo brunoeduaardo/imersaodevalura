037ce140883ac1c5eb71259c6853a033c2fa681a
Bruno Eduardo <brumo1234@gmail.com>
2021-09-14 19:24:56 -0300

    Algumas alterações e exercícios na aula 1 e criação da base da aula 2

--- Aula 1/js/script.js
+++ Aula 1/js/script.js
@@ -1,12 +1,21 @@
 var nome = 'Bruno';
-var primeiraNota = 9;
-var segundaNota = 8;
+var primeiraNota = 8;
+var segundaNota = 9;
 var terceiraNota = 9;
-var quartaNota = 5;
+var quartaNota = 9;
 var notaFinal = (primeiraNota + segundaNota + terceiraNota
 + quartaNota)/4;
 
 var notaArredondada = notaFinal.toFixed(1)
 
+const nota = document.getElementById('nota');
+
+if(notaArredondada >= 6){
+    alert('Aluno aprovado, com nota: ' + notaArredondada);
+} else {
+    alert('Aluno reprovado, com nota: ' + notaArredondada);
+}
+
 console.log('Bem vindo ' + nome);
 console.log(notaArredondada);
+
