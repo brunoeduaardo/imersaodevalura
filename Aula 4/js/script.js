eef18335ccf10950eee4ae6f4fedd479a8688793
Bruno Eduardo <brumo1234@gmail.com>
2021-09-16 20:25:37 -0300

    Criação aula 4

---
+++ Aula 4/js/script.js
@@ -0,0 +1,11 @@
+let listaFilmes = [
+    'De volta para o futuro', 
+    'Escola de Rock', 
+    'Mortal Kombat'
+];
+
+
+// no lugar do p, poderia ser usado também strong, b, h1, h2, etc
+document.write('<p>' + listaFilmes[0] + '</p>');
+document.write('<p>' + listaFilmes[1] + '</p>');
+document.write('<p>' + listaFilmes[2] + '</p>');
