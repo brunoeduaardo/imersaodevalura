11799579555cfd91f42557d8ae3a52e294985949
Bruno Eduardo <brumo1234@gmail.com>
2021-09-16 21:00:06 -0300

    Imprimir array com laço for

--- Aula 4/js/script.js
+++ Aula 4/js/script.js
@@ -4,8 +4,12 @@ let listaFilmes = [
     'Mortal Kombat'
 ];
 
+listaFilmes.push('Harry Potter');
+// comando para adicionar um item no final do array
 
+// document.write('<p>' + listaFilmes[0] + '</p>');
 // no lugar do p, poderia ser usado também strong, b, h1, h2, etc
-document.write('<p>' + listaFilmes[0] + '</p>');
-document.write('<p>' + listaFilmes[1] + '</p>');
-document.write('<p>' + listaFilmes[2] + '</p>');
+
+for (let indice = 0; indice < listaFilmes.length; indice = indice + 1) {
+    document.write('<p>' + listaFilmes[indice] + '</p>');
+}
