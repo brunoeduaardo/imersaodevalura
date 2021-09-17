79bd65aca55f0bf36f6235ff452d7b2de4c0976d
Bruno Eduardo <brumo1234@gmail.com>
2021-09-16 21:20:58 -0300

    Criação parte 2 - aula 4

--- Aula 4/Parte 1/js/script.js
+++ Aula 4/Parte 1/js/script.js
@@ -5,11 +5,12 @@ let listaFilmes = [
 ];
 
 listaFilmes.push('Harry Potter');
+listaFilmes.push('Yesterday')
 // comando para adicionar um item no final do array
 
 // document.write('<p>' + listaFilmes[0] + '</p>');
 // no lugar do p, poderia ser usado também strong, b, h1, h2, etc
 
-for (let indice = 0; indice < listaFilmes.length; indice = indice + 1) {
-    document.write('<p>' + listaFilmes[indice] + '</p>');
+for (let indice = 0; indice < listaFilmes.length; indice++) {
+    document.write('<p>' + indice + '<br>' + listaFilmes[indice] + '</p>');
 }
