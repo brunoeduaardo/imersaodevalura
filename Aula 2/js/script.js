01b98dbf6f79817b9a6853df1db95e9ecfc5a82f
Bruno Eduardo <brumo1234@gmail.com>
2021-09-19 14:45:04 -0300

    Adição exercícios aula 2

--- Aula 2/js/script.js
+++ Aula 2/js/script.js
@@ -4,10 +4,25 @@ function Converter(){
     var valorNumericoEmDolar = parseFloat(valor);
 
     var valorEmReal = valorNumericoEmDolar * 5;
+    var valorEmBitcoin = valorNumericoEmDolar * 0.000021;
     console.log(valorEmReal)
 
     var elementoValorConvertido = document.getElementById('valorConvertido');
-    var valorConvertido = 'O resultado em Real é R$ ' + valorEmReal
+    var valorConvertidoEmReal = 'O resultado em Real é R$ ' + valorEmReal;
+    var valorConvertidoEmBTC = 'O resultado em BTC é: ' + valorEmBitcoin + ' BTC';
 
-    elementoValorConvertido.innerHTML = valorConvertido;
+    elementoValorConvertido.innerHTML = valorConvertidoEmReal + '<br>' + valorConvertidoEmBTC;
 }
+
+function ConverterAnos(){
+    var valorElementoAnos = document.getElementById('valorAnos');
+    var valorAnos = valorElementoAnos.value;
+    var valorEmKM = valorAnos * 9460536068.016;
+
+    var elementoValorAnosConvertido = document.getElementById('valorConvertidoAnos');
+    var valorConvertidoEmKM = 'O resultado é igual a: ' + valorEmKM +'Km';
+
+    elementoValorAnosConvertido.innerHTML = valorConvertidoEmKM;
+
+}
+
