150d52dafd4754f6571b9962d1583a54650b6b6a
Bruno Eduardo <brumo1234@gmail.com>
2021-09-19 15:25:21 -0300

    Jogo da aula 3 funcionando, com número de chances limitado e reiniciando automaticamente

--- Aula 3/js/script.js
+++ Aula 3/js/script.js
@@ -1,13 +1,29 @@
+
+function Refresh(){
+    window.location.reload()
+}
+
 var numeroSecreto = parseInt(Math.random() * 11);
+var numeroDeTentativas = 4;
+console.log(numeroSecreto)
 
 function Chutar() {
     var elementoResultado = document.getElementById('resultado');
     var chute = parseInt(document.getElementById('valor').value);
+    var numeroDeTentativasDecrementado = numeroDeTentativas--;
+    console.log(numeroDeTentativasDecrementado);
+
     if (numeroSecreto == chute) {
         elementoResultado.innerHTML = 'Você acertou!';
+    } else if (numeroDeTentativasDecrementado == -1){
+        Refresh();
     } else if (chute > 10 || chute < 0) {
         elementoResultado.innerHTML = 'Escreva um número de 0 a 10';
-    } else {
+    } else if (numeroDeTentativasDecrementado == 0){
         elementoResultado.innerHTML = 'Você errou, o número secreto era: ' + numeroSecreto;
-    }
+    } else if (numeroDeTentativasDecrementado == 1){
+        elementoResultado.innerHTML = 'Tente novamente, você tem mais ' + numeroDeTentativasDecrementado + ' chance';
+    } else if (numeroDeTentativasDecrementado <= 4){
+        elementoResultado.innerHTML = 'Tente novamente, você tem mais ' + numeroDeTentativasDecrementado + ' chances';
+    } 
 }
