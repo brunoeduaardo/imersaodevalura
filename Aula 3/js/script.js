04655ad3bc9ba9d8eb53173e1eb3afec60d1b27f
Bruno Eduardo <brumo1234@gmail.com>
2021-09-19 22:36:41 -0300

    Incrementado desafio aula 3

--- Aula 3/js/script.js
+++ Aula 3/js/script.js
@@ -7,23 +7,36 @@ var numeroSecreto = parseInt(Math.random() * 11);
 var numeroDeTentativas = 4;
 console.log(numeroSecreto)
 
+
 function Chutar() {
     var elementoResultado = document.getElementById('resultado');
     var chute = parseInt(document.getElementById('valor').value);
     var numeroDeTentativasDecrementado = numeroDeTentativas--;
     console.log(numeroDeTentativasDecrementado);
+    
+    if (numeroDeTentativasDecrementado == -1){
+        Refresh(); 
 
-    if (numeroSecreto == chute) {
+    } else if (numeroSecreto == chute) {
         elementoResultado.innerHTML = 'Você acertou!';
-    } else if (numeroDeTentativasDecrementado == -1){
-        Refresh();
+
     } else if (chute > 10 || chute < 0) {
         elementoResultado.innerHTML = 'Escreva um número de 0 a 10';
+
     } else if (numeroDeTentativasDecrementado == 0){
         elementoResultado.innerHTML = 'Você errou, o número secreto era: ' + numeroSecreto;
-    } else if (numeroDeTentativasDecrementado == 1){
-        elementoResultado.innerHTML = 'Tente novamente, você tem mais ' + numeroDeTentativasDecrementado + ' chance';
-    } else if (numeroDeTentativasDecrementado <= 4){
-        elementoResultado.innerHTML = 'Tente novamente, você tem mais ' + numeroDeTentativasDecrementado + ' chances';
+
+    } else if (numeroDeTentativasDecrementado == 1 && numeroSecreto > chute){
+        elementoResultado.innerHTML = 'Tente novamente, você tem mais ' + numeroDeTentativasDecrementado + ' chance' + '<br>O número secreto é maior que seu chute';
+
+    } else if (numeroDeTentativasDecrementado == 1 && numeroSecreto < chute){
+        elementoResultado.innerHTML = 'Tente novamente, você tem mais ' + numeroDeTentativasDecrementado + ' chance' + '<br>O número secreto é menor que seu chute';
+
+    } else if (numeroDeTentativasDecrementado <= 4 && numeroSecreto < chute){
+        elementoResultado.innerHTML = 'Tente novamente, você tem mais ' + numeroDeTentativasDecrementado + ' chances' + '<br>O número secreto é menor que seu chute';
+
+    } else if (numeroDeTentativasDecrementado <= 4 && numeroSecreto > chute){
+        elementoResultado.innerHTML = 'Tente novamente, você tem mais ' + numeroDeTentativasDecrementado + ' chances' + '<br>O número secreto é maior que seu chute';
+        
     } 
 }
