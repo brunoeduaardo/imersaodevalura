622347eba190f9496f43b0fb86ba80b7feed2559
Bruno Eduardo <brumo1234@gmail.com>
2021-09-20 20:47:09 -0300

    Adicionado funcionalidade aos botões

--- Aula 6/js/script.js
+++ Aula 6/js/script.js
@@ -1,16 +1,16 @@
 let rafa = {
     nome: 'Rafa',
-    vitorias: 2,
-    empates: 1,
-    derrotas: 1,
+    vitorias: 0,
+    empates: 0,
+    derrotas: 0,
     pontos: 0
 };
 
 let paulo = {
     nome: 'Paulo',
-    vitorias: 1,
-    empates: 1,
-    derrotas: 2,
+    vitorias: 0,
+    empates: 0,
+    derrotas: 0,
     pontos: 0
 };
 
@@ -32,9 +32,9 @@ function exibeJogadores(jogadores){
         elemento += '<td>' + jogadores[i].empates + '</td>'
         elemento += '<td>' + jogadores[i].derrotas + '</td>'
         elemento += '<td>' + jogadores[i].pontos + '</td>'
-        elemento += "<td><button onClick='adicionarVitoria()'>Vitória</button></td>"
-        elemento += "<td><button onClick='adicionarEmpate()'>Empate</button></td>"
-        elemento += "<td><button onClick='adicionarDerrota()'>Derrota</button></td>"
+        elemento += "<td><button onClick='adicionarVitoria(" + i + ")'>Vitória</button></td>"
+        elemento += "<td><button onClick='adicionarEmpate(" + i + ")'>Empate</button></td>"
+        elemento += "<td><button onClick='adicionarDerrota(" + i + ")'>Derrota</button></td>"
         elemento += '</tr>'
     }
     let tabelaJogadores = document.getElementById('tabelaJogadores');
@@ -42,3 +42,24 @@ function exibeJogadores(jogadores){
 }
 
 exibeJogadores(jogadores);
+
+function adicionarVitoria(i){
+    let jogador = jogadores[i]
+    jogador.vitorias++
+    jogador.pontos = calculaPontos(jogador)
+    exibeJogadores(jogadores)
+}
+
+function adicionarEmpate(i){
+    let jogador = jogadores[i]
+    jogador.empates++
+    jogador.pontos = calculaPontos(jogador)
+    exibeJogadores(jogadores)
+}
+
+function adicionarDerrota(i){
+    let jogador = jogadores[i]
+    jogador.derrotas++
+    jogador.pontos = calculaPontos(jogador)
+    exibeJogadores(jogadores)
+}
