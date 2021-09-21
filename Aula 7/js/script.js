ba64a9cc0bda46456bd322184aa3ac3bbe790ad6
Bruno Eduardo <brumo1234@gmail.com>
2021-09-21 20:49:10 -0300

    Botões funcionando

--- Aula 7/js/script.js
+++ Aula 7/js/script.js
@@ -32,4 +32,44 @@ let cartas = [ // array com cartas criadas
 let cartaMaquina; // definindo duas variáveis vazias para podermos sortear as cartas
 let cartaJogador; // de cada jogador
 
-function sortearCarta()
+function sortearCarta(){
+    let indiceCartaMaquina = parseInt(Math.random() * 3);
+    cartaMaquina = cartas[indiceCartaMaquina];
+
+    let indiceCartaJogador = parseInt(Math.random() * 3);
+    while (indiceCartaJogador == indiceCartaMaquina){
+        indiceCartaJogador = parseInt(Math.random() * 3);
+    }
+    cartaJogador = cartas[indiceCartaJogador];
+
+    document.getElementById('btnSortear').disabled = true;
+    document.getElementById('btnJogar').disabled = false;
+
+    exibirOpcoes();
+}
+
+function exibirOpcoes(){
+    let opcoes = document.getElementById('opcoes');
+    let opcoesTexto = ''
+
+    for (let atributo in cartaJogador.atributos){
+        opcoesTexto += '<input type="radio" name="atributo" value="' + atributo + '">' + atributo;
+    }
+    opcoes.innerHTML = opcoesTexto;
+}
+
+function obtemAtributoSelecionado(){
+    let radioAtributos = document.getElementsByName('atributo');
+
+    for(let i = 0; i < radioAtributos.length; i++){
+        if(radioAtributos[i].checked == true){  
+            return radioAtributos[i].value
+        }
+    }
+}
+
+function jogar(){
+    let atributoSelecionado = obtemAtributoSelecionado();
+    console.log(atributoSelecionado);
+
+}
